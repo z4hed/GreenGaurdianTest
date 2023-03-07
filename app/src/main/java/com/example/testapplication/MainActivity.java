c36427c5d0c10e73f90a4a94401785c80d25947e
z4hed <zhedthegreat@gmail.com>
2023-03-07 11:13:05 -0500

    Login View and Register View added

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_page);
+
+        Button registerButton = findViewById(R.id.register_button);
+        Button loginButton = findViewById(R.id.login_button);
+
+        EditText UserName= findViewById(R.id.login_username);
+        EditText Password = findViewById(R.id.login_password);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //specifying current and target activity
+                Intent moveToRegister = new Intent(MainActivity.this, register_page.class);
+                startActivity(moveToRegister);
+            }
+        });
     }
 }
