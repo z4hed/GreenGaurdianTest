6e200afc2dd584485d3365c825122f7722f09ac0
z4hed <zhedthegreat@gmail.com>
2023-03-07 12:16:23 -0500

    profile object construction in register page completed

---
+++ app/src/main/java/Models/profile.java
@@ -0,0 +1,20 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class profile {
+    private String firstName,lastName;
+    private String userName, password;
+    private String email;
+
+    //plant related data
+    ArrayList<String> plantsOwned;
+
+    public profile(String firstName, String lastName, String userName, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+}
--- app/src/main/java/com/example/testapplication/register_page.java
+++ app/src/main/java/com/example/testapplication/register_page.java
@@ -3,12 +3,45 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import Models.profile;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class register_page extends AppCompatActivity {
+
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
+    private EditText userName;
+    private EditText password;
+    private profile userProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+
+        firstName = findViewById(R.id.register_first_name);
+        lastName = findViewById(R.id.register_last_name);
+        email = findViewById(R.id.regitser_email);
+        userName = findViewById(R.id.register_username);
+        password = findViewById(R.id.register_password);
+
+        //On Click Specifiers
+        Button confirmRegistration = findViewById(R.id.register_register_button);
+        confirmRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String firstNameEntered = firstName.getText().toString();
+                String lastNameEntered = lastName.getText().toString();
+                String emailEntered = email.getText().toString();
+                String userNameEntered = userName.getText().toString();
+                String passwordEntered = password.getText().toString();
+                userProfile = new profile(firstNameEntered,lastNameEntered,userNameEntered,passwordEntered,emailEntered);
+
+            }
+        });
     }
 }
