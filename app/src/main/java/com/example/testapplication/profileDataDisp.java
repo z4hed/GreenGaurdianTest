6d148fe2ca9a50acdc516f678939258320e16963
z4hed <zhedthegreat@gmail.com>
2023-03-07 19:18:15 -0500

    New Test Plant Data Page Completed

--- app/src/main/java/com/example/testapplication/profileDataDisp.java
+++ app/src/main/java/com/example/testapplication/profileDataDisp.java
@@ -3,12 +3,16 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class profileDataDisp extends AppCompatActivity {
 
+    TextView Time, UV, Moisture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_data_disp);
+        Time = findViewById(R.id.input_time_data);
+
     }
 }
